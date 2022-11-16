e472c40603832a1db42c8d90ff68951c3ba3c613
pranitha <bijugapranitha@gmail.com>
2022-11-16 20:09:06 +0530

    Maven Java Project1 push

---
+++ src/main/java/com/mavenweb/MavenJavaProject1/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.MavenJavaProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from PRANITHA-20BD1A6665-CSMB" );
+    }
+}
